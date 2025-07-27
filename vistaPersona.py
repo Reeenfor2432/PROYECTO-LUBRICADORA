494cf72918652c63b11dd8f4b542635ed1ee5eb9
Josue <102492225+Reeenfor2432@users.noreply.github.com>
2025-07-27 18:02:19 -0500

    Update vistaPersona.py

--- vistaPersona.py
+++ vistaPersona.py
@@ -8,16 +8,21 @@ from tkinter import *
 class vistaPersona:
 
     def interfazBase(self, container):
-            
-        #Id
-        labelCe= Label(container, text= "Ingrese identificación:", width=25, font=("arial",12)).grid(row=0, column=0)
+
+        #id
+        labelid = Label(container, text="ID (solo para eliminar):", width=25, font=("arial",12)).grid(row=0, column=0)
+        self.texBoxId = Entry(container)
+        self.texBoxId.grid(row=0, column=1)
+
+        #Cedula
+        labelCe= Label(container, text= "Ingrese identificación:", width=25, font=("arial",12)).grid(row=1, column=0)
         self.texBoxCe= Entry(container)
-        self.texBoxCe.grid(row= 0, column= 1)
+        self.texBoxCe.grid(row= 1, column= 1)
         #Nombre
-        labelNom= Label(container, text= "Ingrese el nombre:", width=25, font=("arial",12)).grid(row=1, column=0)
+        labelNom= Label(container, text= "Ingrese el nombre:", width=25, font=("arial",12)).grid(row=2, column=0)
         self.texBoxNom= Entry(container)
-        self.texBoxNom.grid(row= 1, column= 1)
+        self.texBoxNom.grid(row= 2, column= 1)
         #Telefono
-        labelTel= Label(container, text= "Ingrese número de teléfono:", width=25, font=("arial",12)).grid(row=2, column=0)
+        labelTel= Label(container, text= "Ingrese número de teléfono:", width=25, font=("arial",12)).grid(row=3, column=0)
         self.texBoxTel= Entry(container)
-        self.texBoxTel.grid(row= 2, column= 1)
+        self.texBoxTel.grid(row= 3, column= 1)
