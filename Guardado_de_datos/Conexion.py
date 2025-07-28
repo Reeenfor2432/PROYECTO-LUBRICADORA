f5c3987c3b90a0c26ab163de8bec472d49cf1328
Shemuel <102492225+Reeenfor2432@users.noreply.github.com>
2025-07-27 19:20:52 -0500

    Documentacion de todos los cambios

--- Guardado_de_datos/Conexion.py
+++ Guardado_de_datos/Conexion.py
@@ -1,17 +1,24 @@
-import mysql.connector
+import mysql.connector  # Importa el conector de MySQL para Python
 
 class CConexion:
 
+    # Método para establecer una conexión a la base de datos
     def ConexionBaseDeDatos():
         try:
-            conexion = mysql.connector.connect(user='root',password='LdJGcoDfHWxFQjGunBXIlrVNbQEEVGcI',
-                                               host='gondola.proxy.rlwy.net',database='LUBRICONTROL',
-                                               port='35671')
-            print("La conexion ha sido exitosa!")
-            return conexion
+            # Intentar establecer una conexión a la base de datos con los parámetros proporcionados
+            conexion = mysql.connector.connect(
+                user='root',  # Usuario para la base de datos
+                password='LdJGcoDfHWxFQjGunBXIlrVNbQEEVGcI',  # Contraseña del usuario
+                host='gondola.proxy.rlwy.net',  # Dirección del host de la base de datos
+                database='LUBRICONTROL',  # Nombre de la base de datos
+                port='35671'  # Puerto de la base de datos
+            )
+            print("La conexion ha sido exitosa!")  # Si la conexión es exitosa, se imprime un mensaje
+            return conexion  # Retorna la conexión establecida
 
         except mysql.connector.Error as error:
+            # Si hay un error en la conexión, se captura y se muestra el mensaje de error
             print("Error al conectarte a la base de datos {}".format(error))
-            return conexion
+            return conexion  # Retorna la conexión (aunque en caso de error no se ha creado correctamente)
         
-    ConexionBaseDeDatos()
+    # Llamada al método para establecer la conexión (en este
--- vistaCliente.py
+++ vistaCliente.py
@@ -5,24 +5,25 @@ from Guardado_de_datos.AgregarClientes import Aggclientes
 
 
 class vistaCliente(vistaPersona):
-
+    # Método principal para administrar los clientes, con base en una ventana base
     def administrarCliente(self, base, callback=None):
+        # Crear un cuadro de grupo (LabelFrame) para mostrar los datos del cliente
         groupBox = LabelFrame(base, text="Datos del Cliente", padx=10, pady=10)
         groupBox.pack(pady=10)
 
-        # Crear los campos de entrada desde vistaPersona
+        # Llamar a la interfaz base para crear campos comunes de entrada (heredados de vistaPersona)
         super().interfazBase(groupBox)
 
-        # Botones de acción
+        # Crear botones de acción para añadir, modificar y eliminar registros
         Button(groupBox, text="Añadir", width=15, command=self.guardarRegistros).grid(row=4, column=0)
         Button(groupBox, text="Modificar", width=15, command=self.modificarRegistro).grid(row=4, column=1)
         Button(groupBox, text="Eliminar", width=15, command=self.eliminarRegistro).grid(row=4, column=2, padx=(40, 40))
 
-        # Crear tabla (TreeView)
+        # Crear un frame para mostrar la tabla de clientes
         tablaFrame = LabelFrame(base, text="Lista de Clientes", padx=5, pady=5)
         tablaFrame.pack(pady=10)
 
-
+        # Crear una tabla (Treeview) con las columnas ID, cedula, nombre_cliente y telefono
         self.tabla = ttk.Treeview(tablaFrame, columns=("ID","cedula", "nombre_cliente", "telefono"), show="headings", height=5)
         self.tabla.heading("ID", text="ID")
         self.tabla.heading("cedula", text="cedula")
@@ -30,113 +31,124 @@ class vistaCliente(vistaPersona):
         self.tabla.heading("telefono", text="Teléfono")
         self.tabla.pack()
 
-        # Evento al seleccionar un registro
+        # Vincular el evento de selección de un registro de la tabla
         self.tabla.bind("<<TreeviewSelect>>", self.seleccionarRegistro)
 
-        # Cargar los datos en la tabla
+        # Cargar los datos de la base de datos y actualizarlos en la tabla
         self.actualizarVistaTabla()
 
-        # Botón para volver al menú
+        # Botón para volver al menú principal
         if callback:
             Button(base, text="Volver al menú", command=callback).pack(pady=10)
 
     # Método para guardar registros en la base de datos
     def guardarRegistros(self):
         try:
+            # Obtener los valores de los campos de entrada
             cedula = self.texBoxCe.get()
             nombre = self.texBoxNom.get()
             telefono = self.texBoxTel.get()
 
+            # Llamar a la función para agregar un cliente
             Aggclientes.IngresarClientes(cedula, nombre, telefono)
             messagebox.showinfo("INFORMACIÓN", "Los datos fueron ingresados exitosamente")
 
-            # Limpiar campos
+            # Limpiar los campos de entrada después de guardar
             self.texBoxCe.delete(0, END)
             self.texBoxNom.delete(0, END)
             self.texBoxTel.delete(0, END)
 
-            # Actualizar tabla
+            # Actualizar la vista de la tabla
             self.actualizarVistaTabla()
 
         except ValueError as error:
+            # Si hay un error, se imprime en la consola
             print("Error al ingresar los datos: {}".format(error))
 
-    # Método para actualizar la vista de la tabla
+    # Método para actualizar la vista de la tabla de clientes
     def actualizarVistaTabla(self):
         try:
-            self.tabla.delete(*self.tabla.get_children())  # limpiar tabla
+            # Limpiar la tabla antes de cargar los nuevos datos
+            self.tabla.delete(*self.tabla.get_children())
 
+            # Obtener los datos de los clientes desde la base de datos
             datos = Aggclientes.MostrarClientes()
+
+            # Insertar cada fila de datos en la tabla
             for row in datos:
                 self.tabla.insert("", "end", values=row)
 
         except Exception as error:
+            # Si hay un error, se imprime en la consola
             print("Error al actualizar tabla: {}".format(error))
 
-    #Para seleccionar un registro de la tabla y mostrarlo en los campos de entrada (DANGER - NO TOCAR)
+    # Método para seleccionar un registro de la tabla y mostrarlo en los campos de entrada
     def seleccionarRegistro(self, event):
         try:
-            item = self.tabla.focus()
+            item = self.tabla.focus()  # Obtener el registro seleccionado
             if item:
-                values = self.tabla.item(item)['values']
+                values = self.tabla.item(item)['values']  # Obtener los valores del registro seleccionado
 
+                # Insertar los valores seleccionados en los campos de entrada
                 self.texBoxId.delete(0, END)
                 self.texBoxId.insert(0, values[0])
                 
                 self.texBoxCe.delete(0, END)
-                self.texBoxCe.insert(0, f"{int(values[1]):010d}")  # 10 dígitos
-                
+                self.texBoxCe.insert(0, f"{int(values[1]):010d}")  # 10 dígitos en formato
                 self.texBoxNom.delete(0, END)
                 self.texBoxNom.insert(0, values[2])
                 
                 self.texBoxTel.delete(0, END)
-                self.texBoxTel.insert(0, f"{int(values[3]):010d}")  # 10 dígitos
+                self.texBoxTel.insert(0, f"{int(values[3]):010d}")  # 10 dígitos en formato
         except Exception as error:
+            # Si hay un error, se imprime en la consola
             print("Error al seleccionar:", error)
 
+    # Método para modificar un registro existente en la base de datos
     def modificarRegistro(self):
         try:
+            # Obtener los valores de los campos de entrada
             id = self.texBoxId.get()
             cedula = self.texBoxCe.get()
             nombre = self.texBoxNom.get()
             telefono = self.texBoxTel.get()
 
-
-            Aggclientes.ModificarClientes(id,cedula, nombre, telefono)
+            # Llamar a la función para modificar los datos del cliente
+            Aggclientes.ModificarClientes(id, cedula, nombre, telefono)
             messagebox.showinfo("INFORMACIÓN", "Los datos fueron modificados exitosamente")
 
-            # Limpiar camposs
+            # Limpiar los campos de entrada después de modificar
             self.texBoxId.delete(0, END)
             self.texBoxCe.delete(0, END)
             self.texBoxNom.delete(0, END)
             self.texBoxTel.delete(0, END)
 
-            # Actualizar tabla
+            # Actualizar la vista de la tabla
             self.actualizarVistaTabla()
 
         except ValueError as error:
+            # Si hay un error, se imprime en la consola
             print("Error al modificar los datos: {}".format(error))
 
-
+    # Método para eliminar un registro de la base de datos
     def eliminarRegistro(self):
         try:
+            # Obtener el ID del cliente a eliminar
             id = self.texBoxId.get()
-        
 
+            # Llamar a la función para eliminar el cliente
             Aggclientes.EliminarClientes(id)
             messagebox.showinfo("INFORMACIÓN", "Los datos fueron eliminados exitosamente")
 
-            # Limpiar campos
+            # Limpiar los campos de entrada después de eliminar
             self.texBoxId.delete(0, END)
             self.texBoxCe.delete(0, END)
             self.texBoxNom.delete(0, END)
             self.texBoxTel.delete(0, END)
 
-            # Actualizar tabla
+            # Actualizar la vista de la tabla
             self.actualizarVistaTabla()
 
         except ValueError as error:
+            # Si hay un error, se imprime en la consola
             print("Error al modificar los datos: {}".format(error))
-
-        
-
